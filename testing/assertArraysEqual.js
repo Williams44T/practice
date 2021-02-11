b45dff05c5c5957d751148bb3f48eb936abe5450
Travis Williams <williams44t@gmail.com>
2021-02-11 15:09:31 -0800

    creates testing helper function to assert arrays as equal

---
+++ testing/assertArraysEqual.js
@@ -0,0 +1,22 @@
+var assertArraysEqual = (actual, expected, testName) => {
+  var sameLength = actual.length === expected.length;
+  var sameElements = true;
+
+  if (sameLength) {
+    for (var i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        sameElements = false;
+      }
+    }
+  }
+
+  if (sameLength && sameElements) {
+    console.log('PASS:', testName);
+  } else {
+    console.log('FAIL:', testName);
+    console.log('   EXPECTED:', expected);
+    console.log('   ACTUAL  :', actual);
+  }
+};
+
+module.exports = assertArraysEqual;
